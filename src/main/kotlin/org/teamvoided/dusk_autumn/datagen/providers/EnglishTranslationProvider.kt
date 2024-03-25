7c7f0376d936e10ab0b4b30eece0ea1a7bbd025f
theendercore <theendercore@gmail.com>
2024-03-25 16:35:44 +0200

    - fixed en_lang

--- src/main/kotlin/org/teamvoided/dusk_autumn/datagen/providers/EnglishTranslationProvider.kt
+++ src/main/kotlin/org/teamvoided/dusk_autumn/datagen/providers/EnglishTranslationProvider.kt
@@ -12,14 +12,14 @@ import org.teamvoided.dusk_autumn.init.DuskItems
 class EnglishTranslationProvider(o: FabricDataOutput) : FabricLanguageProvider(o) {
 
     val items = listOf(DuskItems.CASCADE_SAPLING)
-    val blocks = listOf(DuskBlocks.CASCADE_SAPLING)
+    val blocks = listOf<Block>()
     override fun generateTranslations(gen: TranslationBuilder) {
         items.forEach { gen.add(it.translationKey, genLang(it.id)) }
         blocks.forEach { gen.add(it.translationKey, genLang(it.id)) }
     }
 
     private fun genLang(identifier: Identifier): String =
-        identifier.path.split("_").joinToString("") { it.replaceFirstChar(Char::uppercaseChar) }
+        identifier.path.split("_").joinToString(" ") { it.replaceFirstChar(Char::uppercaseChar) }
 
     val Item.id get() = Registries.ITEM.getId(this)
     val Block.id get() = Registries.BLOCK.getId(this)
