9908a14fa0c4087c99345788249d2d639a53d778
theendercore <theendercore@gmail.com>
2024-03-26 18:05:52 +0200

    - did the loot table func

--- src/main/kotlin/org/teamvoided/dusk_autumn/datagen/providers/BlockLootTableProvider.kt
+++ src/main/kotlin/org/teamvoided/dusk_autumn/datagen/providers/BlockLootTableProvider.kt
@@ -4,18 +4,13 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider
 import net.minecraft.block.Block
 import net.minecraft.block.Blocks
-import net.minecraft.block.SnowBlock
 import net.minecraft.data.server.loot_table.VanillaBlockLootTableGenerator.JUNGLE_SAPLING_DROP_CHANCES
-import net.minecraft.item.Items
 import net.minecraft.loot.LootPool
 import net.minecraft.loot.LootTable
 import net.minecraft.loot.condition.BlockStatePropertyLootCondition
-import net.minecraft.loot.condition.EntityPropertiesLootCondition
-import net.minecraft.loot.context.LootContext
 import net.minecraft.loot.entry.AlternativeEntry
 import net.minecraft.loot.entry.ItemEntry
-import net.minecraft.loot.entry.LeafEntry
-import net.minecraft.loot.entry.LootPoolEntry
+import net.minecraft.loot.entry.LootTableEntry
 import net.minecraft.loot.function.SetCountLootFunction
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider
 import net.minecraft.predicate.StatePredicate
@@ -38,35 +33,43 @@ class BlockLootTableProvider(o: FabricDataOutput) : FabricBlockLootTableProvider
         add(DuskBlocks.JUNGLE_LEAF_PILE) { leavesDrops(it, Blocks.JUNGLE_SAPLING, *JUNGLE_SAPLING_DROP_CHANCES) }
         add(DuskBlocks.MANGROVE_LEAF_PILE, ::mangroveLeavesDrops)
 
-        add(DuskBlocks.CASCADE_LEAF_PILE) {
-            LootTable.builder().pool(
-                LootPool.builder().with(
-                    AlternativeEntry.builder(
-                        LeafPileBlock.PILE_LAYERS.values
-                    ) { integer: Int? ->
-                        if (integer == 4) ItemEntry.builder(
-                            DuskBlocks.CASCADE_LEAF_PILE
-                        ) else ItemEntry.builder(Blocks.SNOW).apply(
-                            SetCountLootFunction.builder(
-                                ConstantLootNumberProvider.create(
-                                    integer!!.toFloat()
-                                )
-                            )
-                        ).conditionally(
-                            BlockStatePropertyLootCondition.builder(it).properties(
-                                StatePredicate.Builder.create().exactMatch(
-                                    LeafPileBlock.PILE_LAYERS,
-                                    integer
-                                )
-                            )
-                        )
-                    }
-                )
-            )
-        }
+        add(DuskBlocks.CASCADE_LEAF_PILE) { leafPile(it, DuskBlocks.CASCADE_LEAVES) }
 
         add(DuskBlocks.BLUE_PETALS, ::method_49358)
 
         add(DuskBlocks.POTTED_VIOLET_DAISY, ::pottedPlantDrops)
+    }
+
+    private fun constantLootNumber(i: Number): ConstantLootNumberProvider =
+        ConstantLootNumberProvider.create(i.toFloat())
+
+    fun leafPile(pile: Block, leaves: Block): LootTable.Builder {
+        return LootTable.builder().pool(
+            LootPool.builder().with(
+                AlternativeEntry.builder(
+                    AlternativeEntry.builder(LeafPileBlock.PILE_LAYERS.values) { layers ->
+                        if (layers == 4) LootTableEntry.builder(leaves.lootTableId)
+                        else
+                        //NEEDS TO BE CHANGED !!!!!! (idk what you want here)
+                            ItemEntry.builder(pile)
+                                .apply(SetCountLootFunction.builder(constantLootNumber(layers)))
+                                .conditionally(
+                                    BlockStatePropertyLootCondition.builder(pile).properties(
+                                        StatePredicate.Builder.create().exactMatch(LeafPileBlock.PILE_LAYERS, layers)
+                                    )
+                                )
+                    },
+                    AlternativeEntry.builder(LeafPileBlock.PILE_LAYERS.values) { layers ->
+                        ItemEntry.builder(pile)
+                            .apply(SetCountLootFunction.builder(constantLootNumber(layers)))
+                            .conditionally(
+                                BlockStatePropertyLootCondition.builder(pile).properties(
+                                    StatePredicate.Builder.create().exactMatch(LeafPileBlock.PILE_LAYERS, layers)
+                                )
+                            )
+                    }.conditionally(WITH_SHEARS_OR_SILK_TOUCH)
+                )
+            )
+        )
     }
 }
