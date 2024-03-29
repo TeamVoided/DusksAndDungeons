96c874e4177175d109e432f7c503b0b4386ca670
TheDwindlingLight <genericperson1080@gmail.com>
2024-03-29 00:27:48 -0400

    the changes

--- src/main/kotlin/org/teamvoided/dusk_autumn/item/FarmersHat.kt
+++ src/main/kotlin/org/teamvoided/dusk_autumn/item/FarmersHat.kt
@@ -1,11 +1,11 @@
 package org.teamvoided.dusk_autumn.item
 
-import net.minecraft.block.CarvedPumpkinBlock
 import net.minecraft.entity.EquipmentSlot
+import net.minecraft.item.DyeableItem
 import net.minecraft.item.Equippable
 import net.minecraft.item.Item
 
-class FarmersHat(settings: Item.Settings) : Item(settings), Equippable {
+class FarmersHat(settings: Item.Settings) : Item(settings), Equippable, DyeableItem {
     override fun getPreferredSlot(): EquipmentSlot {
         return EquipmentSlot.HEAD
     }
