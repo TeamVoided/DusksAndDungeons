b1c4edfed9ea978cdaea6d3cf9b7632cc08ad1e3
theendercore <theendercore@gmail.com>
2024-07-15 22:03:00 +0300

    - doesnt need to inherit

--- src/main/kotlin/org/teamvoided/dusk_autumn/item/DuskFoodComponents.kt
+++ src/main/kotlin/org/teamvoided/dusk_autumn/item/DuskFoodComponents.kt
@@ -1,11 +1,9 @@
 package org.teamvoided.dusk_autumn.item
 
 import net.minecraft.item.FoodComponent
-import net.minecraft.item.FoodComponents
 
-class DuskFoodComponents: FoodComponents(){
-    companion object{
-        val GOLDEN_BEETROOT: FoodComponent = FoodComponent.Builder().hunger(5).saturation(1.6f).build()
-        val MOONBERRIES = FoodComponent.Builder().hunger(4).saturation(0.3f).build()
-    }
+@Suppress("HasPlatformType", "MagicNumber")
+object DuskFoodComponents {
+    val GOLDEN_BEETROOT  = FoodComponent.Builder().hunger(5).saturation(1.6f).build()
+    val MOONBERRIES  = FoodComponent.Builder().hunger(4).saturation(0.3f).build()
 }
