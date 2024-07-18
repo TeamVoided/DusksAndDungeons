9776031a27ee4c06988d9c9fe79c22dedb4c4bed
theendercore <theendercore@gmail.com>
2024-07-18 18:46:21 +0300

    - things

--- src/main/kotlin/org/teamvoided/dusk_autumn/modules/SnifferInjection.kt
+++ src/main/kotlin/org/teamvoided/dusk_autumn/modules/SnifferInjection.kt
@@ -1,6 +1,7 @@
 package org.teamvoided.dusk_autumn.modules
 
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents
+import net.minecraft.loot.LootTable
 import net.minecraft.loot.LootTables
 import net.minecraft.loot.entry.LootTableEntry
 import net.minecraft.registry.RegistryKey
@@ -8,10 +9,10 @@ import net.minecraft.registry.RegistryKeys
 import org.teamvoided.dusk_autumn.DuskAutumns.id
 
 object SnifferInjection {
-    val ADD_MOONBERRIES = RegistryKey.of(RegistryKeys.LOOT_TABLE, id("inject/add_moonberries"))
+    private val ADD_MOONBERRIES: RegistryKey<LootTable> = RegistryKey.of(RegistryKeys.LOOT_TABLE, id("inject/add_moonberries"))
 
     fun init() {
-        LootTableEvents.MODIFY.register { key, tableBuilder, _, registries ->
+        LootTableEvents.MODIFY.register { key, tableBuilder, _, _ ->
             if (key == LootTables.SNIFFER_DIGGING_GAMEPLAY)
                 tableBuilder.modifyPools { it.with(LootTableEntry.method_428(ADD_MOONBERRIES)).build() }
         }
