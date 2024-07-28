c3848744a5e6ac19f6b9363faefeba0791e93fa3
TheDwindlingLight <genericperson1080@gmail.com>
2024-07-28 19:16:10 -0400

    added the saplings as wandering trader blocks, and added the cascade highlight signs texture

--- src/main/kotlin/org/teamvoided/dusk_autumn/DuskAutumns.kt
+++ src/main/kotlin/org/teamvoided/dusk_autumn/DuskAutumns.kt
@@ -2,6 +2,7 @@ package org.teamvoided.dusk_autumn
 
 import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
+import org.teamvoided.dusk_autumn.event.TradingEventHandler
 import org.teamvoided.dusk_autumn.init.*
 import org.teamvoided.dusk_autumn.init.worldgen.DuskBiomeModifications
 import org.teamvoided.dusk_autumn.modules.SnifferInjection
@@ -22,6 +23,7 @@ object DuskAutumns {
         DuskParticles.init()
         DuskItemGroups.init()
         SnifferInjection.init()
+        TradingEventHandler.init()
        /* CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
             val test = literal("end").executes {
                 0
