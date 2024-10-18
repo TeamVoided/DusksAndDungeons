fc05a74b1a0ef5e48b775e1b3d3d06a267052430
TheDwindlingLight <genericperson1080@gmail.com>
2024-10-18 17:41:39 -0400

    myxin 2 its fixed

--- src/main/java/org/teamvoided/dusk_autumn/mixin/PersistentProjectileEntityAccessor.java
+++ src/main/java/org/teamvoided/dusk_autumn/mixin/PersistentProjectileEntityAccessor.java
@@ -7,5 +7,5 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(PersistentProjectileEntity.class)
 public interface PersistentProjectileEntityAccessor {
     @Accessor("inGround")
-    public void setInGround(Boolean inGround);
+    public void setInGround(boolean inGround);
 }
