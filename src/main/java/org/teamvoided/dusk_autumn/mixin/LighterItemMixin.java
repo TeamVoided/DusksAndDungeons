77215ba18ec5693942ef17f2814e057ab3138a3e
theendercore <theendercore@gmail.com>
2024-10-23 13:36:57 +0300

    - fixed Lighter Items crashing the game

--- src/main/java/org/teamvoided/dusk_autumn/mixin/LighterItemMixin.java
+++ src/main/java/org/teamvoided/dusk_autumn/mixin/LighterItemMixin.java
@@ -4,16 +4,18 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.CampfireBlock;
 import net.minecraft.item.FireChargeItem;
 import net.minecraft.item.FlintAndSteelItem;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.teamvoided.dusk_autumn.block.CandelabraBlock;
 
+@Debug(export = true)
 @Mixin({FireChargeItem.class, FlintAndSteelItem.class})
-public class FireChargeItemMixin {
+public class LighterItemMixin {
 
     @Redirect(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/CampfireBlock;canBeLit(Lnet/minecraft/block/BlockState;)Z"))
-    boolean canBeLit(BlockState state) {
-        return !CampfireBlock.canBeLit(state) && !CandelabraBlock.canBeLit(state);
+    boolean additionCanBeLitChecks(BlockState state) {
+        return CampfireBlock.canBeLit(state) ^ CandelabraBlock.canBeLit(state);
     }
 }
