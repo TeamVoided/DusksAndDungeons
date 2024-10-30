0beba3439861bb6942b33daa443a34c441c2845b
theendercore <theendercore@gmail.com>
2024-10-30 18:10:12 +0200

    - merged ItemHelper and SecretHelper.kt

--- src/main/kotlin/org/teamvoided/dusk_autumn/util/SecretHelper.kt
+++ src/main/kotlin/org/teamvoided/dusk_autumn/util/SecretHelper.kt
@@ -2,6 +2,8 @@ package org.teamvoided.dusk_autumn.util
 
 import net.minecraft.block.Block
 import net.minecraft.item.Item
+import org.teamvoided.dusk_autumn.init.DnDBlocks
+import org.teamvoided.dusk_autumn.init.DnDItems
 
 val SECRET_BLOCKS = mutableSetOf<Block>()
 val SECRET_ITEMS = mutableSetOf<Item>()
@@ -13,6 +15,16 @@ fun Block.shh(): Block {
 
 fun Item.shh(): Item {
     SECRET_ITEMS.add(this)
+    return this
+}
+
+fun Item.tellWitnessesThatIWasMurdered(): Item {
+    DnDItems.EVIL_ITEMS.add(this)
+    return this
+}
+
+fun Block.tellWitnessesThatIWasMurdered(): Block {
+    DnDBlocks.EVIL_BLOCKS.add(this)
     return this
 }
 
