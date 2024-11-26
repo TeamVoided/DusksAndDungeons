8a17c023eb5880ae789bce937d789c7332c29a17
TheEnderCore <theendercore@gmail.com>
2024-11-26 23:24:26 +0200

    - status effect refactor

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/effect/MadnessEffect.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/effect/MadnessEffect.kt
@@ -3,15 +3,12 @@ package org.teamvoided.dusks_and_dungeons.effect
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.effect.StatusEffectType
 import net.minecraft.particle.ParticleEffect
+import org.teamvoided.voidlib.helpers.effects.VoidStatusEffect
 
-class MadnessEffect : DnDStatusEffect {
+class MadnessEffect : VoidStatusEffect {
     constructor(type: StatusEffectType, color: Int) : super(type, color)
     constructor(type: StatusEffectType, color: Int, particle: ParticleEffect) : super(type, color, particle)
 
-
     override fun shouldApplyUpdateEffect(tick: Int, amplifier: Int): Boolean = true
-
-    override fun applyUpdateEffect(entity: LivingEntity, amplifier: Int): Boolean {
-        return true
-    }
+    override fun applyUpdateEffect(entity: LivingEntity, amplifier: Int): Boolean = true
 }
