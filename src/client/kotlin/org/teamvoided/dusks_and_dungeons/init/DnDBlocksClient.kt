03065ac1467b879669a82825e2a695e114cd56bf
TheEnderCore <theendercore@gmail.com>
2024-11-26 20:01:20 +0200

    - reformated DnDBlocksClient

--- src/client/kotlin/org/teamvoided/dusks_and_dungeons/init/DnDBlocksClient.kt
+++ src/client/kotlin/org/teamvoided/dusks_and_dungeons/init/DnDBlocksClient.kt
@@ -1,8 +1,10 @@
 package org.teamvoided.dusks_and_dungeons.init
 
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
-import net.fabricmc.fabric.api.client.particle.v1.ParticleRenderEvents
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap.INSTANCE as BLOCK_RL_MAP
+import net.fabricmc.fabric.api.client.particle.v1.ParticleRenderEvents.ALLOW_BLOCK_DUST_TINT
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry
+import net.minecraft.block.Block
+import net.minecraft.client.color.block.BlockColorProvider
 import net.minecraft.client.color.world.BiomeColors
 import net.minecraft.client.color.world.FoliageColors
 import net.minecraft.client.color.world.GrassColors
@@ -12,52 +14,42 @@ import net.minecraft.world.BlockRenderView
 import org.teamvoided.dusks_and_dungeons.init.blocks.DnDWoodBlocks
 import org.teamvoided.dusks_and_dungeons.util.DnDBlockLists
 
+
 object DnDBlocksClient {
     fun init() {
-        ColorProviderRegistry.BLOCK.register(
-            { _, world, pos, _ ->
-                foliageColorOrDefault(world, pos)
-            },
+        register(
+            { _, world, pos, _ -> foliageColor(world, pos) },
             DnDWoodBlocks.OAK_LEAF_PILE,
             DnDWoodBlocks.JUNGLE_LEAF_PILE,
             DnDWoodBlocks.ACACIA_LEAF_PILE,
             DnDWoodBlocks.DARK_OAK_LEAF_PILE,
             DnDWoodBlocks.MANGROVE_LEAF_PILE
         )
-        ColorProviderRegistry.BLOCK.register(
-            { _, world, pos, tintIndex ->
-                if (tintIndex != 0) {
-                    grassColorOrDefault(world, pos)
-                } else -1
-            },
+        register(
+            { _, world, pos, tintIndex -> if (tintIndex != 0) grassColor(world, pos) else -1 },
             *DnDBlockLists.flowerbedBlocks.toTypedArray()
         )
-        ColorProviderRegistry.BLOCK.register(
-            { _, world, pos, _ -> grassColorOrDefault(world, pos) },
-            *DnDBlocks.GRASS_TINT_BLOCKS.toTypedArray()
-        )
-        ColorProviderRegistry.BLOCK.register(
-            { _, _, _, _ -> FoliageColors.getSpruceColor() }, DnDWoodBlocks.SPRUCE_LEAF_PILE
-        )
-        ColorProviderRegistry.BLOCK.register(
-            { _, _, _, _ -> FoliageColors.getBirchColor() }, DnDWoodBlocks.BIRCH_LEAF_PILE
-        )
+        register({ _, world, pos, _ -> grassColor(world, pos) }, *DnDBlocks.GRASS_TINT_BLOCKS.toTypedArray())
+        register(FoliageColors.getSpruceColor(), DnDWoodBlocks.SPRUCE_LEAF_PILE)
+        register(FoliageColors.getBirchColor(), DnDWoodBlocks.BIRCH_LEAF_PILE)
 
-        DnDBlocks.CUTOUT_BLOCKS.forEach { BlockRenderLayerMap.INSTANCE.putBlock(it, RenderLayer.getCutout()) }
-        DnDBlocks.TRANSLUCENT_BLOCKS.forEach { BlockRenderLayerMap.INSTANCE.putBlock(it, RenderLayer.getTranslucent()) }
+        DnDBlocks.CUTOUT_BLOCKS.forEach { BLOCK_RL_MAP.putBlock(it, RenderLayer.getCutout()) }
+        DnDBlocks.TRANSLUCENT_BLOCKS.forEach { BLOCK_RL_MAP.putBlock(it, RenderLayer.getTranslucent()) }
 
-
-        ParticleRenderEvents.ALLOW_BLOCK_DUST_TINT.register { blockState, _, _ ->
-            blockState.block !in DnDBlocks.GRASS_TINT_BLOCKS
-        }
+        ALLOW_BLOCK_DUST_TINT.register { state, _, _ -> state.block !in DnDBlocks.GRASS_TINT_BLOCKS }
     }
 
-    private fun grassColorOrDefault(world: BlockRenderView?, pos: BlockPos?): Int {
+    fun register(provider: BlockColorProvider, vararg blocks: Block) =
+        ColorProviderRegistry.BLOCK.register(provider, *blocks)
+
+    fun register(tint: Int, vararg blocks: Block) = register({ _, _, _, _ -> tint }, *blocks)
+
+    fun grassColor(world: BlockRenderView?, pos: BlockPos?): Int {
         return if (world != null && pos != null) BiomeColors.getGrassColor(world, pos)
         else GrassColors.getDefault()
     }
 
-    private fun foliageColorOrDefault(world: BlockRenderView?, pos: BlockPos?): Int {
+    fun foliageColor(world: BlockRenderView?, pos: BlockPos?): Int {
         return if (world != null && pos != null) BiomeColors.getFoliageColor(world, pos)
         else FoliageColors.getColor(0.8, 0.4)
     }
