c18d8394b5600fb738304d5db21f6ff34ddcefa8
TheEnderCore <theendercore@gmail.com>
2025-02-09 14:56:17 +0200

    - added extension functions for BlockConvertable

--- src/main/kotlin/org/teamvoided/voidlib/helpers/block/BlockConvertable.kt
+++ src/main/kotlin/org/teamvoided/voidlib/helpers/block/BlockConvertable.kt
@@ -4,8 +4,15 @@ package org.teamvoided.voidlib.helpers.block
 
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.item.BlockItem
 
 interface BlockConvertable {
     fun asBlock(): Block
     fun getDefaultState(): BlockState
 }
+
+fun Block.asBlock(): Block = this
+fun BlockItem.asBlock(): Block = this.block
+fun BlockItem.getDefaultState(): BlockState = this.block.defaultState
+fun BlockState.asBlock(): Block = this.block
+fun BlockState.getDefaultState(): BlockState = this.block.defaultState
