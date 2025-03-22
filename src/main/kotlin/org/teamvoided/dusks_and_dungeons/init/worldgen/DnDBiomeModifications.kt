cc365ec116d43a5633e539c55ebb3ebdbb4f3a20
TheEnderCore <theendercore@gmail.com>
2025-03-22 02:13:21 +0200

    - clean

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/init/worldgen/DnDBiomeModifications.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/init/worldgen/DnDBiomeModifications.kt
@@ -1,14 +1,13 @@
 package org.teamvoided.dusks_and_dungeons.init.worldgen
 
-import net.fabricmc.fabric.api.biome.v1.BiomeModificationContext
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors
 import net.fabricmc.fabric.api.biome.v1.ModificationPhase
-import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBiomeTags
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBiomeTags as Tags
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.tag.TagKey
 import net.minecraft.world.biome.Biome
-import net.minecraft.world.gen.GenerationStep
+import net.minecraft.world.gen.GenerationStep.Feature
 import net.minecraft.world.gen.feature.PlacedFeature
 import org.teamvoided.dusks_and_dungeons.DusksAndDungeons.id
 import org.teamvoided.dusks_and_dungeons.data.worldgen.DnDPlacedFeature
@@ -16,42 +15,20 @@ import org.teamvoided.dusks_and_dungeons.data.worldgen.DnDPlacedFeature
 
 object DnDBiomeModifications {
     fun init() {
-        addFeature(
-            "add_shallow_stoney_ore",
-            GenerationStep.Feature.UNDERGROUND_ORES,
-            DnDPlacedFeature.ROCKY_ORE_UPPER,
-            ConventionalBiomeTags.IS_OVERWORLD
-        )
-        addFeature(
-            "add_deep_stoney_ore",
-            GenerationStep.Feature.UNDERGROUND_ORES,
-            DnDPlacedFeature.ROCKY_ORE_LOWER,
-            ConventionalBiomeTags.IS_OVERWORLD
-        )
-        addFeature(
-            "add_slated_ore",
-            GenerationStep.Feature.UNDERGROUND_ORES,
-            DnDPlacedFeature.SLATED_ORE,
-            ConventionalBiomeTags.IS_OVERWORLD
-        )
-        addFeature(
-            "add_blackstoned_ore",
-            GenerationStep.Feature.UNDERGROUND_ORES,
-            DnDPlacedFeature.BLACKSTONED_ORE,
-            ConventionalBiomeTags.IS_NETHER
-        )
+        addOres("shallow_stoney_ore", DnDPlacedFeature.ROCKY_ORE_UPPER, Tags.IS_OVERWORLD)
+        addOres("deep_stoney_ore", DnDPlacedFeature.ROCKY_ORE_LOWER, Tags.IS_OVERWORLD)
+        addOres("slated_ore", DnDPlacedFeature.SLATED_ORE, Tags.IS_OVERWORLD)
+        addOres("blackstoned_ore", DnDPlacedFeature.BLACKSTONED_ORE, Tags.IS_NETHER)
     }
 
+    private fun addOres(id: String, placedFeature: RegistryKey<PlacedFeature>, biome: TagKey<Biome>) =
+        addFeature(id, Feature.UNDERGROUND_ORES, placedFeature, biome)
+
+    @Suppress("SameParameterValue")
     private fun addFeature(
-        id: String,
-        generationStep: GenerationStep.Feature,
-        placedFeature: RegistryKey<PlacedFeature>,
-        biome: TagKey<Biome>
-    ) {
-        BiomeModifications.create(id(id)).add(
-            ModificationPhase.ADDITIONS, BiomeSelectors.tag(biome)
-        ) { context: BiomeModificationContext ->
-            context.generationSettings.addFeature(generationStep, placedFeature)
-        }
+        id: String, generationStep: Feature, placedFeature: RegistryKey<PlacedFeature>, biome: TagKey<Biome>
+    ) = BiomeModifications.create(id("add_$id")).add(ModificationPhase.ADDITIONS, BiomeSelectors.tag(biome)) { it ->
+        it.generationSettings.addFeature(generationStep, placedFeature)
     }
+
 }
