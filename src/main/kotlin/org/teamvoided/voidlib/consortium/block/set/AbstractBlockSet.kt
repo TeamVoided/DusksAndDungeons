d6879b8d4d1f3a03421c484e5234f24cf56dc0a0
TheEnderCore <theendercore@gmail.com>
2025-03-23 00:03:33 +0200

    - HasFamily interface

--- src/main/kotlin/org/teamvoided/voidlib/consortium/block/set/AbstractBlockSet.kt
+++ src/main/kotlin/org/teamvoided/voidlib/consortium/block/set/AbstractBlockSet.kt
@@ -2,9 +2,12 @@ package org.teamvoided.voidlib.consortium.block.set
 
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.data.family.BlockFamilies
+import net.minecraft.data.family.BlockFamily
 import net.minecraft.item.Item
 import net.minecraft.item.ItemConvertible
 import org.teamvoided.voidlib.consortium.block.BlockCollection
+import org.teamvoided.voidlib.consortium.utils.HasFamily
 import org.teamvoided.voidlib.helpers.block.BlockConvertable
 import java.util.function.Supplier
 
@@ -12,7 +15,7 @@ abstract class AbstractBlockSet(
     val name: String,
     val parent: Block, val stairs: Block, val slab: Block, val wall: Block,
     val hasStoneCutting: Boolean
-) : ItemConvertible, Supplier<Block>, BlockConvertable, BlockCollection<Block> {
+) : ItemConvertible, Supplier<Block>, BlockConvertable, BlockCollection<Block>, HasFamily {
     override val list: List<Block> = listOf(stairs, slab, wall)
     override fun getIdMap(): Map<String, Block> =
         mapOf("${name}_stairs" to stairs, "${name}_slab" to slab, "${name}_wall" to wall)
@@ -21,4 +24,6 @@ abstract class AbstractBlockSet(
     override fun get() = parent
     override fun asBlock(): Block = parent
     override fun getDefaultState(): BlockState = parent.defaultState
+
+    override val family: BlockFamily = BlockFamilies.register(parent).stairs(stairs).slab(slab).wall(wall).build()
 }
---
+++ src/main/kotlin/org/teamvoided/voidlib/consortium/utils/HasFamily.kt
@@ -0,0 +1,7 @@
+package org.teamvoided.voidlib.consortium.utils
+
+import net.minecraft.data.family.BlockFamily
+
+interface HasFamily {
+    val family: BlockFamily
+}
