d48b60906dde0e5094791b58f02c4bb426b07d2b
TheEnderCore <theendercore@gmail.com>
2025-03-23 16:00:08 +0200

    - ShadowAttributeMixin is now better

---
+++ src/client/java/org/teamvoided/dusks_and_dungeons/mixin/ShadowAttributeMixin.java
@@ -0,0 +1,25 @@
+package org.teamvoided.dusks_and_dungeons.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.teamvoided.dusks_and_dungeons.init.DnDItems;
+
+@Mixin(EntityRenderDispatcher.class)
+public class ShadowAttributeMixin {
+
+    @ModifyExpressionValue(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/EntityRenderer;method_55831(Lnet/minecraft/entity/Entity;)F"))
+    float shadowAttribute(float original, Entity entity) {
+        var shadowSize = original;
+        if (entity instanceof LivingEntity living) {
+            for (ItemStack it : living.getAllArmorItems()) {
+                if (it.isOf(DnDItems.VILE_WITCH_HAT)) shadowSize *= 2;
+            }
+        }
+        return shadowSize;
+    }
+}
