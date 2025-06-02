81edebf971676f3d44f01a2d3e58e0f0bdf9c9e8
TheEnderCore <theendercore@gmail.com>
2025-06-03 01:00:10 +0300

    Fixed bug #5

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/block/GoldenBeetrootsBlock.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/block/GoldenBeetrootsBlock.kt
@@ -6,10 +6,12 @@ import net.minecraft.item.ItemConvertible
 import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.random.RandomGenerator
+import net.minecraft.world.WorldView
 import org.teamvoided.dusks_and_dungeons.init.DnDItems
 
 class GoldenBeetrootsBlock(settings: Settings) : BeetrootsBlock(settings) {
     override fun getSeedsItem(): ItemConvertible = DnDItems.GOLDEN_BEETROOT
+    override fun isFertilizable(world: WorldView?, pos: BlockPos?, state: BlockState?): Boolean = false
     override fun randomTick(state: BlockState, world: ServerWorld, pos: BlockPos, random: RandomGenerator) {
         if (random.nextInt(2) != 0) super.randomTick(state, world, pos, random)
     }
