0e444d7afbbe579810851a09991d1879dd642914
TheEnderCore <theendercore@gmail.com>
2026-05-24 02:20:29 +0300

    fix log and api not being java static

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/api/BlockStrippingRegistry.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/api/BlockStrippingRegistry.kt
@@ -1,14 +1,15 @@
 package org.teamvoided.dusks_and_dungeons.api
 
-import com.mojang.text2speech.Narrator
 import net.minecraft.world.level.block.Block
+import org.teamvoided.dusks_and_dungeons.DusksAndDungeons.log
 import org.teamvoided.dusks_and_dungeons.impl.BlockStrippingRegistryIml
 
 object BlockStrippingRegistry {
+    @JvmStatic
     fun register(input: Block, stripped: Block) {
         val old = BlockStrippingRegistryIml.BLOCK_STATE_MAP.put(input, stripped)
         if (old != null) {
-            Narrator.LOGGER.debug("Replaced old stripping mapping from {} to {} with {}", input, old, stripped)
+            log.debug("Replaced old stripping mapping from {} to {} with {}", input, old, stripped)
         }
     }
 }
