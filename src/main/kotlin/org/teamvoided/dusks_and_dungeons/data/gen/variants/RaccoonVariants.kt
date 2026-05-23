23a6cb590d9dd802fa7aee33ef9205b424b2e7dc
TheEnderCore <theendercore@gmail.com>
2026-05-23 21:31:53 +0300

    fixed raccoon textures

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/data/gen/variants/RaccoonVariants.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/data/gen/variants/RaccoonVariants.kt
@@ -1,13 +1,17 @@
 package org.teamvoided.dusks_and_dungeons.data.gen.variants
 
 import net.minecraft.registry.BootstrapContext
-import org.teamvoided.dusks_and_dungeons.DusksAndDungeons
+import net.minecraft.util.Identifier
+import org.teamvoided.dusks_and_dungeons.DusksAndDungeons.id
 import org.teamvoided.dusks_and_dungeons.entity.RaccoonVariant
 import org.teamvoided.dusks_and_dungeons.init.DnDRaccoonVariants
 
 object RaccoonVariants {
 
     fun bootstrap(c: BootstrapContext<RaccoonVariant>) {
-        c.register(DnDRaccoonVariants.DEFAULT, RaccoonVariant(DusksAndDungeons.id("textures/entity/raccoon.png")))
+        c.register(DnDRaccoonVariants.DEFAULT, RaccoonVariant(raccoon("raccoon")))
     }
+
+    fun raccoon(name: String): Identifier = id("textures/entity/raccoon/$name.png")
+
 }
