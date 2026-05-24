4e905aa423b52c662571c28d53952676428ed886
TheEnderCore <theendercore@gmail.com>
2026-05-24 21:19:53 +0300

    Hollow Logs

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/block/HollowLogWithCuttingBlock.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/block/HollowLogWithCuttingBlock.kt
@@ -1,31 +1,35 @@
 package org.teamvoided.dusks_and_dungeons.block
 
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.state.BlockState
-import net.minecraft.world.level.block.PipeBlock
-import net.minecraft.world.phys.shapes.CollisionContext
-import net.minecraft.world.entity.player.Player
-import net.minecraft.world.level.material.Fluids
-import net.minecraft.world.item.ItemStack
-import net.minecraft.tags.ItemTags
-import net.minecraft.sounds.SoundSource
-import net.minecraft.sounds.SoundEvents
-import net.minecraft.world.level.block.state.StateDefinition
-import net.minecraft.world.level.block.state.properties.BooleanProperty
-import net.minecraft.world.level.block.state.properties.BlockStateProperties
-import net.minecraft.world.level.block.state.properties.Property
-import net.minecraft.world.InteractionHand
-import net.minecraft.world.ItemInteractionResult
-import net.minecraft.world.phys.BlockHitResult
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
-import net.minecraft.world.phys.Vec3
-import net.minecraft.world.phys.shapes.VoxelShape
-import net.minecraft.world.phys.shapes.Shapes
+import net.minecraft.sounds.SoundEvents
+import net.minecraft.sounds.SoundSource
+import net.minecraft.tags.ItemTags
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.ItemInteractionResult
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.BlockGetter
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.PipeBlock
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.BooleanProperty
 import net.minecraft.world.level.gameevent.GameEvent
+import net.minecraft.world.level.material.Fluids
+import net.minecraft.world.phys.BlockHitResult
+import net.minecraft.world.phys.Vec3
+import net.minecraft.world.phys.shapes.CollisionContext
+import net.minecraft.world.phys.shapes.Shapes
+import net.minecraft.world.phys.shapes.VoxelShape
 import org.teamvoided.dusks_and_dungeons.util.rotateColumn
+import org.teamvoided.voidlib.helpers.mc.isZ
+import org.teamvoided.voidlib.helpers.mc.opposite
+import org.teamvoided.voidlib.helpers.mc.rotateFlat90
+import org.teamvoided.voidlib.helpers.mc.rotateOnAxis
+import kotlin.math.round
 
 open class HollowLogWithCuttingBlock(settings: Properties) : HollowLogBlock(settings) {
     open val special1: Double = 0.0625
@@ -50,134 +54,118 @@ open class HollowLogWithCuttingBlock(settings: Properties) : HollowLogBlock(sett
         pos: BlockPos,
         entity: Player,
         hand: InteractionHand,
-        hitResult: BlockHitResult
+        hitResult: BlockHitResult,
     ): ItemInteractionResult {
-        val getHit = this.getRelativeHitCoordinates(hitResult, state, pos)
-        return if (getHit != null && !stack.isEmpty && entity.abilities.mayBuild && stack.`is`(ItemTags.AXES)) {
+        // TODO rewrite this so you do the less expense checks first
+        val hitState = this.getHitState(hitResult, state, pos)
+        return if (hitState != null && !stack.isEmpty && entity.abilities.mayBuild && stack.`is`(ItemTags.AXES)) {
             if (state.getValue(WATERLOGGED)) world.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(world))
-            setBlockState(world, pos, entity, getHit)
+            setBlockState(world, pos, entity, hitState)
             world.neighborChanged(pos, this, pos)
             ItemInteractionResult.sidedSuccess(world.isClientSide)
         } else super.useItemOn(stack, state, world, pos, entity, hand, hitResult)
     }
 
-    private fun getRelativeHitCoordinates(
-        blockHitResult: BlockHitResult, state: BlockState, pos: BlockPos
-    ): BlockState? {
-        if (howManyTrueSides(state) <= 1) return null
-        val vec3d: Vec3 = blockHitResult.location.subtract(pos.x.toDouble(), pos.y.toDouble(), pos.z.toDouble())
-        val directionAxis = state.getValue(AXIS)
-        val north: Direction
-        val east: Direction
-        val up: Direction
-        var directionHit: Direction = blockHitResult.direction
-        when (directionAxis) {
-            Direction.Axis.X -> {
-                north = Direction.NORTH
-                east = Direction.UP
-                up = Direction.WEST
-                if ((directionHit != east && directionHit != east.opposite) &&
-                    (vec3d.y > special1 && vec3d.y < special2) &&
-                    (vec3d.z > special1 && vec3d.z < special2)
-                ) directionHit = directionHit.opposite
-            }
+    fun getHitState(hit: BlockHitResult, state: BlockState, pos: BlockPos): BlockState? {
+        if (howManySidesExist(state) <= 1) return null
+        var vec3 = hit.location.subtract(pos.x.toDouble(), pos.y.toDouble(), pos.z.toDouble())
+        val axis = state.getValue(AXIS)
+        var direction = hit.direction
 
-            Direction.Axis.Y -> {
-                north = Direction.NORTH
-                east = Direction.EAST
-                up = Direction.UP
-                if ((directionHit != up && directionHit != up.opposite) &&
-                    (vec3d.x > special1 && vec3d.x < special2) &&
-                    (vec3d.z > special1 && vec3d.z < special2)
-                ) directionHit = directionHit.opposite
-            }
+        // turns sideways logs upright
+        // assume default state is y-axis
+        if (axis.isHorizontal) {
+            direction = direction.getClockWise(axis.opposite())
+            vec3 = vec3.rotateOnAxis(axis)
+        }
 
-            Direction.Axis.Z -> {
-                north = Direction.DOWN
-                east = Direction.EAST
-                up = Direction.SOUTH
-                if ((directionHit != north && directionHit != north.opposite) &&
-                    (vec3d.x > special1 && vec3d.x < special2) &&
-                    (vec3d.y > special1 && vec3d.y < special2)
-                ) directionHit = directionHit.opposite
-            }
+        val dirAxis = direction.axis
 
-            else -> throw MatchException(
-                "somehow managed to give an invalid axis for hollow logs",
-                null as Throwable?
+        var keyVec: Vec3
+
+        // We round these so that we don't have to check for side length
+        val edgeZ = round(vec3.z)
+        val edgeX = round(vec3.x)
+
+        if (dirAxis.isHorizontal) {
+            // when horizontal keyVec is easy since we just check what direction is clicked
+            // and based on what axis its on we either set x to 0.5 or the edge value
+            keyVec = Vec3(
+                if (dirAxis.isZ()) 0.5 else edgeX,
+                0.5,
+                if (dirAxis.isZ()) edgeZ else 0.5,
             )
+        } else {
+            // for vertical keyVec we do an X chart and add values to x and z based if it falls in one of the 4 sectors
+            var x = 0.0
+            var z = 0.0
+
+            if (vec3.x > vec3.z) x += 0.5
+            if (vec3.x > 1 - vec3.z) x += 0.5
+
+            if (vec3.z > vec3.x) z += 0.5
+            if (vec3.z > 1 - vec3.x) z += 0.5
+
+            keyVec = Vec3(x, 0.5, z)
         }
 
-        when (directionHit) {
-            north -> return stateOrNull(NORTH, state)
-            north.opposite -> return stateOrNull(SOUTH, state)
-            east -> return stateOrNull(EAST, state)
-            east.opposite -> return stateOrNull(WEST, state)
-            up, up.opposite -> {
-                return when (directionAxis) {
-                    Direction.Axis.X -> {
-                        upDownSurfaceCase(vec3d.y, vec3d.z, state)
-                    }
+        // An early escape
+        // we check if the easy options have been taken already and if they have we simply exit
+        // If not we assume that they cant be taken
+        var side = SIDE_MAP[keyVec]
+        if (side != null && state.getValue(side)) {
+            return state.trySetValue(side, false)
+        }
 
-                    Direction.Axis.Y -> {
-                        upDownSurfaceCase(vec3d.x, vec3d.z, state)
-                    }
-
-                    Direction.Axis.Z -> {
-                        upDownSurfaceCase(vec3d.x, vec3d.y, state)
-                    }
+        if (dirAxis.isHorizontal) {
+            // Basically the same check for faces but its now for edges of side faces
+            keyVec = Vec3(
+                if (dirAxis.isZ()) edgeX else 0.5,
+                0.5,
+                if (dirAxis.isZ()) 0.5 else edgeZ,
+            )
+        } else {
+            // This is the biggest mess of the whole code
+            // the point of it is to fix the problem of X shape leave tiny part of the corners not covered when the face is missing
+            // this should be rewritten after I get a break
+            val rots = if (keyVec.x == edgeX) {
+                if (keyVec.x > 0.5) {
+                    if (edgeZ > keyVec.z) 1 else 3
+                } else {
+                    if (edgeZ < keyVec.z) 1 else 3
                 }
-            }
+            } else if (keyVec.z == edgeZ) {
+                if (keyVec.z > 0.5) {
+                    if (edgeX > keyVec.x) 3 else 1
+                } else {
+                    if (edgeX < keyVec.x) 3 else 1
+                }
+            } else 0
 
-            else -> {
-                throw MatchException(
-                    "somehow managed to give an invalid side for hollow logs, thrower is $directionHit, north $north, east $east, up $up",
-                    null as Throwable?
-                )
-            }
+            keyVec = keyVec.rotateFlat90(rots)
         }
+
+        side = SIDE_MAP[keyVec]
+        if (side != null && state.getValue(side)) {
+            return state.trySetValue(side, false)
+        }
+
+        // This should never run so :)
+        return null
     }
 
-    fun upDownSurfaceCase(x: Double, z: Double, state: BlockState): BlockState? {
-        return if (state.getValue(NORTH) == true && x > z && x < (1 - z)) {
-            state.setValue(NORTH, false)
-        } else if (state.getValue(SOUTH) == true && x < z && x > (1 - z)) {
-            state.setValue(SOUTH, false)
-        } else if (state.getValue(EAST) == true && x > z && x > (1 - z)) {
-            state.setValue(EAST, false)
-        } else if (state.getValue(WEST) == true && x < z && x < (1 - z)) {
-            state.setValue(WEST, false)
-        } else null
-    }
-
-    fun stateOrNull(direction: Property<Boolean>, state: BlockState): BlockState? {
-        return if (state.getValue(direction) == false) null
-        else {
-            state.setValue(direction, false)
-        }
-    }
-
-    private fun howManyTrueSides(state: BlockState): Int {
-        return listOf(
-            (state.getValue(NORTH) == true),
-            (state.getValue(SOUTH) == true),
-            (state.getValue(EAST) == true),
-            (state.getValue(WEST) == true)
-        ).count {
-            it
-        }
+    fun howManySidesExist(state: BlockState): Int {
+        return listOf(state.getValue(NORTH), state.getValue(SOUTH), state.getValue(EAST), state.getValue(WEST))
+            .count { it }
     }
 
     private fun setBlockState(
-        world: Level,
-        pos: BlockPos,
-        player: Player,
-        state: BlockState
+        world: Level, pos: BlockPos, player: Player, state: BlockState,
     ) {
         if (!world.isClientSide) {
             val soundEvent = SoundEvents.AXE_STRIP
             world.setBlockAndUpdate(pos, state)
-            world.playSound(null as Player?, pos, soundEvent, SoundSource.BLOCKS, 1.0f, 1.0f)
+            world.playSound(null, pos, soundEvent, SoundSource.BLOCKS, 1.0f, 1.0f)
             world.gameEvent(player, GameEvent.BLOCK_CHANGE, pos)
         }
     }
@@ -186,13 +174,15 @@ open class HollowLogWithCuttingBlock(settings: Properties) : HollowLogBlock(sett
         state: BlockState,
         world: BlockGetter,
         pos: BlockPos,
-        context: CollisionContext
+        context: CollisionContext,
     ): VoxelShape {
         var shape = Shapes.empty()
+        // TODO turn this in to a map lookup with binary keys
         if (state.getValue(NORTH)) shape = Shapes.or(shape, NORTH_SHAPE)
         if (state.getValue(SOUTH)) shape = Shapes.or(shape, SOUTH_SHAPE)
         if (state.getValue(EAST)) shape = Shapes.or(shape, EAST_SHAPE)
         if (state.getValue(WEST)) shape = Shapes.or(shape, WEST_SHAPE)
+        if (shape.isEmpty) return Shapes.block()
         return shape.rotateColumn(state.getValue(AXIS))
     }
 
@@ -205,18 +195,25 @@ open class HollowLogWithCuttingBlock(settings: Properties) : HollowLogBlock(sett
     }
 
     companion object {
-        val NORTH = BlockStateProperties.NORTH
-        val SOUTH = BlockStateProperties.SOUTH
-        val EAST = BlockStateProperties.EAST
-        val WEST = BlockStateProperties.WEST
-        val DIRECTION_PROPERTIES = PipeBlock.PROPERTY_BY_DIRECTION
-        fun getProperty(direction: Direction): BooleanProperty? {
-            return DIRECTION_PROPERTIES[direction]
-        }
+
+        val NORTH: BooleanProperty = BlockStateProperties.NORTH
+        val SOUTH: BooleanProperty = BlockStateProperties.SOUTH
+        val EAST: BooleanProperty = BlockStateProperties.EAST
+        val WEST: BooleanProperty = BlockStateProperties.WEST
+        val DIRECTION_PROPERTIES: Map<Direction, BooleanProperty> = PipeBlock.PROPERTY_BY_DIRECTION
+        fun getProperty(direction: Direction): BooleanProperty? = DIRECTION_PROPERTIES[direction]
+
+        val SIDE_MAP = mapOf(
+            Vec3(1.0, 0.5, 0.5) to EAST,
+            Vec3(0.0, 0.5, 0.5) to WEST,
+            Vec3(0.5, 0.5, 1.0) to SOUTH,
+            Vec3(0.5, 0.5, 0.0) to NORTH,
+        )
 
         val NORTH_SHAPE: VoxelShape = box(0.0, 0.0, 0.0, 16.0, 16.0, 2.0)
         val SOUTH_SHAPE: VoxelShape = box(0.0, 0.0, 14.0, 16.0, 16.0, 16.0)
         val EAST_SHAPE: VoxelShape = box(14.0, 0.0, 0.0, 16.0, 16.0, 16.0)
         val WEST_SHAPE: VoxelShape = box(0.0, 0.0, 0.0, 2.0, 16.0, 16.0)
+
     }
 }
--- src/main/kotlin/org/teamvoided/voidlib/helpers/mc/Misc.kt
+++ src/main/kotlin/org/teamvoided/voidlib/helpers/mc/Misc.kt
@@ -2,13 +2,14 @@
 
 package org.teamvoided.voidlib.helpers.mc
 
-import net.minecraft.world.item.enchantment.Enchantment
-import net.minecraft.world.entity.EquipmentSlot
-import net.minecraft.world.item.ItemStack
-import net.minecraft.world.level.storage.loot.LootTable
+import net.minecraft.core.Direction
 import net.minecraft.resources.ResourceKey
 import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionHand
+import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.enchantment.Enchantment
+import net.minecraft.world.level.storage.loot.LootTable
 import net.minecraft.world.phys.Vec3
 
 fun InteractionHand.toSlot() = if (this == InteractionHand.MAIN_HAND) EquipmentSlot.MAINHAND else EquipmentSlot.OFFHAND
@@ -26,4 +27,23 @@ fun Vec3.rotateFlat90(times: Int): Vec3 {
     return vector
 }
 
-fun ServerLevel.getLootTable(key: ResourceKey<LootTable>): LootTable = this.server.reloadableRegistries().getLootTable(key)
+fun Vec3.rotateOnAxis(axis: Direction.Axis): Vec3 = when (axis) {
+    Direction.Axis.X -> Vec3(y, 1 - x, z)
+    Direction.Axis.Y -> Vec3(1 - z, y, x)
+    Direction.Axis.Z -> Vec3(x, 1 - z, y)
+}
+
+fun Direction.Axis.opposite() = when (this) {
+    Direction.Axis.X -> Direction.Axis.Z
+    Direction.Axis.Y -> Direction.Axis.Y
+    Direction.Axis.Z -> Direction.Axis.X
+}
+
+fun Direction.Axis.isX() = this == Direction.Axis.X
+fun Direction.Axis.isY() = this == Direction.Axis.Y
+fun Direction.Axis.isZ() = this == Direction.Axis.Z
+
+fun Vec3.asString(): String = "(x:${x}, z:${z})"
+
+fun ServerLevel.getLootTable(key: ResourceKey<LootTable>): LootTable =
+    this.server.reloadableRegistries().getLootTable(key)
