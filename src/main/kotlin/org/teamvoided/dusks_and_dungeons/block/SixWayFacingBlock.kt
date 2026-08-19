8145c00bd6d2f5a0701470193ccb7b82741a5c94
TheEnderCore <theendercore@gmail.com>
2026-08-19 12:46:58 +0300

    SixWayFacingBlock cleanup

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/block/SixWayFacingBlock.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/block/SixWayFacingBlock.kt
@@ -1,31 +1,38 @@
 package org.teamvoided.dusks_and_dungeons.block
 
 import com.mojang.serialization.MapCodec
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.core.Direction
 import net.minecraft.world.item.context.BlockPlaceContext
-import net.minecraft.world.level.block.state.StateDefinition
-import net.minecraft.world.level.block.state.properties.DirectionProperty
-import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Mirror
 import net.minecraft.world.level.block.Rotation
-import net.minecraft.core.Direction
+import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.DirectionProperty
 
 open class SixWayFacingBlock(settings: Properties) : Block(settings) {
-    public override fun codec(): MapCodec<out SixWayFacingBlock> = CODEC
 
     init {
-        this.registerDefaultState(defaultBlockState().setValue(FACING, Direction.UP))
+        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.UP))
     }
 
-    override fun getStateForPlacement(ctx: BlockPlaceContext): BlockState = defaultBlockState().setValue(FACING, ctx.clickedFace)
-    override fun rotate(state: BlockState, rotation: Rotation): BlockState =
-        state.setValue(FACING, rotation.rotate(state.getValue(FACING)))
+    public override fun codec(): MapCodec<out SixWayFacingBlock> = CODEC
 
-    override fun mirror(state: BlockState, mirror: Mirror): BlockState =
-        state.rotate(mirror.getRotation(state.getValue(FACING)))
+    override fun getStateForPlacement(ctx: BlockPlaceContext): BlockState? {
+        return super.getStateForPlacement(ctx)?.setValue(FACING, ctx.clickedFace)
+    }
+
+    override fun rotate(state: BlockState, rotation: Rotation): BlockState {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)))
+    }
+
+    override fun mirror(state: BlockState, mirror: Mirror): BlockState {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)))
+    }
 
     override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
+        super.createBlockStateDefinition(builder)
         builder.add(FACING)
     }
 
