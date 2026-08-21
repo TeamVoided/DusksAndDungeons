845a45063bf354f5faab59c65fe8aa1e8c35fdcd
TheEnderCore <theendercore@gmail.com>
2026-08-21 14:05:22 +0300

    clean up DnDItemsClient

--- src/client/kotlin/org/teamvoided/dusks_and_dungeons/init/DnDItemsClient.kt
+++ src/client/kotlin/org/teamvoided/dusks_and_dungeons/init/DnDItemsClient.kt
@@ -5,27 +5,26 @@ import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry
 import net.minecraft.ChatFormatting
 import net.minecraft.client.color.item.ItemColor
 import net.minecraft.network.chat.Component
-import net.minecraft.world.item.Item
 import net.minecraft.world.item.component.DyedItemColor
 import net.minecraft.world.level.FoliageColor
 import net.minecraft.world.level.GrassColor
+import net.minecraft.world.level.ItemLike
 import org.teamvoided.dusks_and_dungeons.util.block.GRASS_TINT_BLOCKS
 
 object DnDItemsClient {
+
     fun init() {
-        registerTint(
-            { _, _ -> GrassColor.getDefaultColor() }, *GRASS_TINT_BLOCKS.map { it.asItem() }.toTypedArray()
-        )
+        registerTint({ _, _ -> GrassColor.getDefaultColor() }, *GRASS_TINT_BLOCKS.toTypedArray())
         registerTint(
             FoliageColor.getDefaultColor(),
-            DnDBlocks.OAK_LEAF_PILE.asItem(),
-            DnDBlocks.JUNGLE_LEAF_PILE.asItem(),
-            DnDBlocks.ACACIA_LEAF_PILE.asItem(),
-            DnDBlocks.DARK_OAK_LEAF_PILE.asItem()
+            DnDBlocks.OAK_LEAF_PILE,
+            DnDBlocks.JUNGLE_LEAF_PILE,
+            DnDBlocks.ACACIA_LEAF_PILE,
+            DnDBlocks.DARK_OAK_LEAF_PILE
         )
-        registerTint(FoliageColor.getEvergreenColor(), DnDBlocks.SPRUCE_LEAF_PILE.asItem())
-        registerTint(FoliageColor.getBirchColor(), DnDBlocks.BIRCH_LEAF_PILE.asItem())
-        registerTint(FoliageColor.getMangroveColor(), DnDBlocks.MANGROVE_LEAF_PILE.asItem())
+        registerTint(FoliageColor.getEvergreenColor(), DnDBlocks.SPRUCE_LEAF_PILE)
+        registerTint(FoliageColor.getBirchColor(), DnDBlocks.BIRCH_LEAF_PILE)
+        registerTint(FoliageColor.getMangroveColor(), DnDBlocks.MANGROVE_LEAF_PILE)
         registerTint({ stack, _ -> DyedItemColor.getOrDefault(stack, 0xffffff) }, DnDItems.FARMERS_HAT)
 
         ItemTooltipCallback.EVENT.register { stack, _, _, lines ->
@@ -39,9 +38,9 @@ object DnDItemsClient {
         }
     }
 
-    fun registerTint(provider: ItemColor, vararg items: Item) =
+    fun registerTint(tint: Int, vararg items: ItemLike) = registerTint({ _, _ -> tint }, *items)
+    fun registerTint(provider: ItemColor, vararg items: ItemLike) {
         ColorProviderRegistry.ITEM.register(provider, *items)
-
-    fun registerTint(tint: Int, vararg items: Item) = registerTint({ _, _ -> tint }, *items)
+    }
 
 }
