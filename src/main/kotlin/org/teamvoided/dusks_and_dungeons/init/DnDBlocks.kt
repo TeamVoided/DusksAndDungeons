eae0888a66ef5c03b8aa0dd0148137375a069218
TheEnderCore <theendercore@gmail.com>
2026-08-23 00:01:42 +0300

    fix

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/init/DnDBlocks.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/init/DnDBlocks.kt
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.registry.FlammableBlockRegistry
 import net.fabricmc.fabric.api.registry.StrippableBlockRegistry
 import net.minecraft.core.particles.ParticleTypes
 import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.sounds.SoundEvents
 import net.minecraft.util.ColorRGBA
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.Item
@@ -13,6 +14,7 @@ import net.minecraft.world.level.block.state.BlockBehaviour.OffsetType
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties.ofFullCopy
 import net.minecraft.world.level.block.state.properties.BlockSetType
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument
 import net.minecraft.world.level.material.MapColor
 import net.minecraft.world.level.material.PushReaction
 import org.teamvoided.dusks_and_dungeons.DusksAndDungeons.id
