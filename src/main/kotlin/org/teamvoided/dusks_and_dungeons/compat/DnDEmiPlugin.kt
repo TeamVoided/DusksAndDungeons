44220022b94867a4064148c2c388c7ef77acbf07
TheEnderCore <theendercore@gmail.com>
2026-08-24 19:51:30 +0300

    cleaned up plugin

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/compat/DnDEmiPlugin.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/compat/DnDEmiPlugin.kt
@@ -3,22 +3,11 @@ package org.teamvoided.dusks_and_dungeons.compat
 import dev.emi.emi.api.EmiPlugin
 import dev.emi.emi.api.EmiRegistry
 import dev.emi.emi.api.recipe.EmiRecipe
-import dev.emi.emi.api.recipe.EmiWorldInteractionRecipe
-import dev.emi.emi.api.stack.EmiIngredient
 import dev.emi.emi.api.stack.EmiStack
-import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags
-import net.minecraft.core.component.DataComponents
-import net.minecraft.core.registries.BuiltInRegistries
-import net.minecraft.core.registries.Registries
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.Items
-import net.minecraft.world.item.alchemy.Potion
-import net.minecraft.world.item.alchemy.PotionContents
 import net.minecraft.world.item.alchemy.Potions
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.material.Fluid
 import net.minecraft.world.level.material.Fluids
 import org.teamvoided.dusks_and_dungeons.DusksAndDungeons.MODID
 import org.teamvoided.dusks_and_dungeons.DusksAndDungeons.id
@@ -27,8 +16,6 @@ import org.teamvoided.dusks_and_dungeons.DusksAndDungeons.log
 import org.teamvoided.dusks_and_dungeons.impl.BlockStrippingRegistryIml
 import org.teamvoided.dusks_and_dungeons.init.DnDItems
 import org.teamvoided.dusks_and_dungeons.init.DnDItems.EVIL_ITEMS
-import org.teamvoided.dusks_and_dungeons.util.tag
-import kotlin.jvm.optionals.getOrNull
 
 object DnDEmiPlugin : EmiPlugin {
 
@@ -47,8 +34,7 @@ object DnDEmiPlugin : EmiPlugin {
     }
 
     fun addWorldInteraction(registry: EmiRegistry, hiddenItems: Set<Item>) {
-
-        val axes: EmiIngredient = getAxes()
+        val axes = getAxes()
         for ((from, to) in BlockStrippingRegistryIml.BLOCK_STATE_MAP) {
             val input = EmiStack.of(from)
             val output = EmiStack.of(to)
@@ -61,7 +47,6 @@ object DnDEmiPlugin : EmiPlugin {
         }
 
         val water = EmiStack.of(Fluids.WATER, 81_000)
-
         registry.addRecipeSafe(synthetic("world/unique", "$MODID/tinted_water_bottle")) { id ->
             basicWorld(
                 EmiStack.of(DnDItems.TINTED_GLASS_BOTTLE), water,
@@ -69,64 +54,9 @@ object DnDEmiPlugin : EmiPlugin {
                 id
             )
         }
-
     }
 
-    fun getAxes(): EmiIngredient {
-        return damagedTool(
-            getPreferredTag(
-                mutableListOf(
-                    "minecraft:axes", "c:axes", "c:tools/axes", "fabric:axes", "forge:tools/axes"
-                ),
-                EmiStack.of(Items.IRON_AXE)
-            ), 1
-        )
-    }
-
-    fun damagedTool(tool: EmiIngredient, damage: Int): EmiIngredient {
-        for (stack in tool.emiStacks) {
-            val rawStack = stack.itemStack.copy()
-            rawStack.damageValue = damage
-            stack.remainder = EmiStack.of(rawStack)
-        }
-        return tool
-    }
-
-
-    fun getPreferredTag(candidates: MutableList<String>, fallback: EmiIngredient): EmiIngredient {
-        for (id in candidates) {
-            val potential = EmiIngredient.of(Registries.ITEM.tag(ResourceLocation.parse(id)))
-            if (!potential.isEmpty) {
-                return potential
-            }
-        }
-        return fallback
-    }
-
-
-    fun subId(id: ResourceLocation): String = "${id.namespace}/${id.path}"
-    fun subId(block: Block): String = subId(BuiltInRegistries.BLOCK.getKey(block))
-    fun subId(item: Item): String = subId(BuiltInRegistries.ITEM.getKey(item))
-    fun subId(fluid: Fluid): String = subId(BuiltInRegistries.FLUID.getKey(fluid))
-
-    fun setPotion(stack: ItemStack, potion: Potion): ItemStack {
-        stack.update(
-            DataComponents.POTION_CONTENTS, PotionContents.EMPTY,
-            BuiltInRegistries.POTION.wrapAsHolder(potion), PotionContents::withPotion
-        )
-        return stack
-    }
-
-    fun basicWorld(
-        left: EmiIngredient, right: EmiIngredient, output: EmiStack, id: ResourceLocation, catalyst: Boolean = true,
-    ): EmiRecipe {
-        return EmiWorldInteractionRecipe.builder()
-            .id(id)
-            .leftInput(left)
-            .rightInput(right, catalyst)
-            .output(output)
-            .build()
-    }
+    fun synthetic(type: String, name: String): ResourceLocation = id("/$type/$name")
 
     fun safely(name: String, runnable: Runnable) {
         try {
@@ -142,17 +72,6 @@ object DnDEmiPlugin : EmiPlugin {
         } catch (e: Throwable) {
             log.warn("Exception thrown when parsing EMI recipe ($id)", e)
         }
-    }
-
-    fun synthetic(type: String, name: String): ResourceLocation = id("/$type/$name")
-
-    fun hiddenFromRecipeViewers(): Set<Item> {
-        return BuiltInRegistries.ITEM.getTag(ConventionalItemTags.HIDDEN_FROM_RECIPE_VIEWERS)
-            .getOrNull()
-            ?.stream()
-            ?.map { it.value() }
-            ?.toList()
-            ?.toSet() ?: setOf()
     }
 
 }
---
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/compat/EmiUtils.kt
@@ -0,0 +1,85 @@
+package org.teamvoided.dusks_and_dungeons.compat
+
+import dev.emi.emi.api.recipe.EmiRecipe
+import dev.emi.emi.api.recipe.EmiWorldInteractionRecipe
+import dev.emi.emi.api.stack.EmiIngredient
+import dev.emi.emi.api.stack.EmiStack
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags
+import net.minecraft.core.component.DataComponents
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.tags.TagKey
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
+import net.minecraft.world.item.alchemy.Potion
+import net.minecraft.world.item.alchemy.PotionContents
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.material.Fluid
+import org.teamvoided.dusks_and_dungeons.util.tag
+import kotlin.jvm.optionals.getOrNull
+
+val AXE_TAGS = mutableListOf(
+    "minecraft:axes",
+    "c:axes",
+    "c:tools/axes",
+    "fabric:axes",
+    "forge:tools/axes"
+).map { Registries.ITEM.tag(ResourceLocation.parse(it)) }
+
+fun getAxes(): EmiIngredient = damagedTool(getPreferredTag(AXE_TAGS, EmiStack.of(Items.IRON_AXE)), 1)
+
+fun damagedTool(tool: EmiIngredient, damage: Int): EmiIngredient {
+    for (stack in tool.emiStacks) {
+        val rawStack = stack.itemStack.copy()
+        rawStack.damageValue = damage
+        stack.remainder = EmiStack.of(rawStack)
+    }
+    return tool
+}
+
+fun getPreferredTag(candidates: List<TagKey<Item>>, fallback: EmiIngredient): EmiIngredient {
+    for (tag in candidates) {
+        val ingredient = EmiIngredient.of(tag)
+        if (!ingredient.isEmpty) {
+            return ingredient
+        }
+    }
+    return fallback
+}
+
+
+fun subId(id: ResourceLocation): String = "${id.namespace}/${id.path}"
+fun subId(block: Block): String = subId(BuiltInRegistries.BLOCK.getKey(block))
+fun subId(item: Item): String = subId(BuiltInRegistries.ITEM.getKey(item))
+fun subId(fluid: Fluid): String = subId(BuiltInRegistries.FLUID.getKey(fluid))
+
+fun setPotion(stack: ItemStack, potion: Potion): ItemStack {
+    stack.update(
+        DataComponents.POTION_CONTENTS, PotionContents.EMPTY,
+        BuiltInRegistries.POTION.wrapAsHolder(potion), PotionContents::withPotion
+    )
+    return stack
+}
+
+fun basicWorld(
+    left: EmiIngredient, right: EmiIngredient, output: EmiStack, id: ResourceLocation, catalyst: Boolean = true,
+): EmiRecipe {
+    return EmiWorldInteractionRecipe.builder()
+        .id(id)
+        .leftInput(left)
+        .rightInput(right, catalyst)
+        .output(output)
+        .build()
+}
+
+
+fun hiddenFromRecipeViewers(): Set<Item> {
+    return BuiltInRegistries.ITEM.getTag(ConventionalItemTags.HIDDEN_FROM_RECIPE_VIEWERS)
+        .getOrNull()
+        ?.stream()
+        ?.map { it.value() }
+        ?.toList()
+        ?.toSet() ?: setOf()
+}
