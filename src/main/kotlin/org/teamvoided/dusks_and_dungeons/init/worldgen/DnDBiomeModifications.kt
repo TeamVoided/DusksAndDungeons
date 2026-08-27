75b34e7a8bbe0ed7ebf75fdd36ebc3abaa7ca5a2
TheEnderCore <theendercore@gmail.com>
2026-08-27 19:49:38 +0300

    cleaned up DnDBiomeModifications a bit

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/init/worldgen/DnDBiomeModifications.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/init/worldgen/DnDBiomeModifications.kt
@@ -1,7 +1,7 @@
 package org.teamvoided.dusks_and_dungeons.init.worldgen
 
-import net.fabricmc.fabric.api.biome.v1.BiomeModifications
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications.create
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors.tag
 import net.fabricmc.fabric.api.biome.v1.ModificationPhase
 import net.minecraft.data.worldgen.placement.VegetationPlacements
 import net.minecraft.resources.ResourceKey
@@ -12,7 +12,6 @@ import net.minecraft.world.level.levelgen.placement.PlacedFeature
 import org.teamvoided.dusks_and_dungeons.DusksAndDungeons.id
 import org.teamvoided.dusks_and_dungeons.data.tags.DnDBiomeTags
 import org.teamvoided.dusks_and_dungeons.data.worldgen.DnDPlacedFeature
-import net.fabricmc.fabric.api.tag.convention.v2.ConventionalBiomeTags as CTags
 
 
 object DnDBiomeModifications {
@@ -20,21 +19,13 @@ object DnDBiomeModifications {
     fun init() {
         addVegetation("golden_mushrooms_cave", DnDPlacedFeature.GOLDEN_MUSHROOM_CAVE, DnDBiomeTags.GOLD_MUSHROOMS_CAVE)
         addVegetation(
-            "golden_mushrooms_surface",
-            DnDPlacedFeature.GOLDEN_MUSHROOM_SURFACE,
-            DnDBiomeTags.GOLD_MUSHROOMS_SURFACE
+            "golden_mushrooms_surface", DnDPlacedFeature.GOLDEN_MUSHROOM_SURFACE, DnDBiomeTags.GOLD_MUSHROOMS_SURFACE
         )
         addVegetation(
-            "golden_mushrooms_common",
-            DnDPlacedFeature.GOLDEN_MUSHROOM_HUGE_PATCH,
-            DnDBiomeTags.GOLD_MUSHROOMS_HUGE
+            "golden_mushrooms_common", DnDPlacedFeature.GOLDEN_MUSHROOM_HUGE_PATCH, DnDBiomeTags.GOLD_MUSHROOMS_HUGE
         )
-
-        replaceVegetation(
-            "mosskin_pumpkins_extra",
-            VegetationPlacements.PATCH_PUMPKIN,
-            DnDPlacedFeature.PATCH_MOSSKIN_PUMPKIN_EXTRA,
-            DnDBiomeTags.MOSSKIN_PUMPKINS_CAVE
+        addVegetation(
+            "mosskin_pumpkins_extra", DnDPlacedFeature.PATCH_MOSSKIN_PUMPKIN_EXTRA, DnDBiomeTags.MOSSKIN_PUMPKINS_CAVE
         )
         replaceVegetation(
             "gloom_pumpkins_extra",
@@ -43,55 +34,29 @@ object DnDBiomeModifications {
             DnDBiomeTags.GLOOM_PUMPKINS_EXTRA
         )
 
-
-        addVegetation(
-            "crimson_warts",
-            DnDPlacedFeature.CRIMSON_WART,
-            DnDBiomeTags.CRIMSON_WART
-        )
-        addVegetation(
-            "warped_warts",
-            DnDPlacedFeature.WARPED_WART,
-            DnDBiomeTags.WARPED_WART
-        )
+        addVegetation("crimson_warts", DnDPlacedFeature.CRIMSON_WART, DnDBiomeTags.CRIMSON_WART)
+        addVegetation("warped_warts", DnDPlacedFeature.WARPED_WART, DnDBiomeTags.WARPED_WART)
     }
 
-    internal fun addOres(id: String, placedFeature: ResourceKey<PlacedFeature>, biome: TagKey<Biome>) {
-        addFeature(id, Decoration.UNDERGROUND_ORES, placedFeature, biome)
+    internal fun addVegetation(id: String, feature: ResourceKey<PlacedFeature>, biome: TagKey<Biome>) {
+        addFeature(id, Decoration.VEGETAL_DECORATION, feature, biome)
     }
 
-    internal fun addVegetation(id: String, placedFeature: ResourceKey<PlacedFeature>, biome: TagKey<Biome>) {
-        addFeature(id, Decoration.VEGETAL_DECORATION, placedFeature, biome)
-    }
-
-    internal fun addFeature(
-        id: String, generationStep: Decoration, placedFeature: ResourceKey<PlacedFeature>, biome: TagKey<Biome>,
-    ) {
-        BiomeModifications.create(id("add_$id")).add(ModificationPhase.ADDITIONS, BiomeSelectors.tag(biome)) {
-            it.generationSettings.addFeature(generationStep, placedFeature)
+    internal fun addFeature(id: String, step: Decoration, feature: ResourceKey<PlacedFeature>, tag: TagKey<Biome>) {
+        create(id("add_$id")).add(ModificationPhase.ADDITIONS, tag(tag)) {
+            it.generationSettings.addFeature(step, feature)
         }
     }
-
 
     internal fun replaceVegetation(
-        id: String,
-        oldPlacedFeature: ResourceKey<PlacedFeature>,
-        newPlacedFeature: ResourceKey<PlacedFeature>,
-        biome: TagKey<Biome>
+        id: String, oldFeature: ResourceKey<PlacedFeature>, newFeature: ResourceKey<PlacedFeature>, tag: TagKey<Biome>,
     ) {
-        replaceFeature(id, Decoration.VEGETAL_DECORATION, oldPlacedFeature, newPlacedFeature, biome)
-    }
-
-    internal fun replaceFeature(
-        id: String,
-        generationStep: Decoration,
-        oldPlacedFeature: ResourceKey<PlacedFeature>,
-        newPlacedFeature: ResourceKey<PlacedFeature>,
-        biome: TagKey<Biome>,
-    ) {
-        BiomeModifications.create(id("replace_$id")).add(ModificationPhase.REPLACEMENTS, BiomeSelectors.tag(biome)) {
-            it.generationSettings.removeFeature(generationStep, oldPlacedFeature)
-            it.generationSettings.addFeature(generationStep, newPlacedFeature)
+        create(id("replace_$id")).add(ModificationPhase.REPLACEMENTS, tag(tag)) { ctx ->
+            with(ctx.generationSettings) {
+                removeFeature(Decoration.VEGETAL_DECORATION, oldFeature)
+                addFeature(Decoration.VEGETAL_DECORATION, newFeature)
+            }
         }
     }
+
 }
