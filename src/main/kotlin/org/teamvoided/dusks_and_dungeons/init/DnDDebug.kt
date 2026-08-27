82362b9221077dd67bfea50e925c3633284f5232
TheEnderCore <theendercore@gmail.com>
2026-08-27 13:46:24 +0300

    spawnParticles(pos, type) debug func

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/init/DnDDebug.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/init/DnDDebug.kt
@@ -2,7 +2,12 @@ package org.teamvoided.dusks_and_dungeons.init
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.fabricmc.fabric.api.event.player.UseBlockCallback
+import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.core.particles.ParticleOptions
+import net.minecraft.core.particles.ParticleTypes
 import net.minecraft.network.chat.Component
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.entity.player.Player
@@ -61,5 +66,22 @@ object DnDDebug {
     }
 
     fun Player.msg(message: String) = displayClientMessage(Component.literal(message), true)
+
+    fun Level.spawnParticles(pos: BlockPos, type: ParticleOptions = ParticleTypes.RAID_OMEN) {
+        if (this !is ServerLevel) return
+        val of = 0.5625
+        val random = this.random
+
+        for (dir in Direction.entries) {
+            val pos2 = pos.relative(dir)
+            if (!getBlockState(pos2).isSolidRender(this, pos2)) {
+                val axis = dir.axis
+                val x = if (axis === Direction.Axis.X) 0.5 + of * dir.stepX else random.nextDouble()
+                val y = if (axis === Direction.Axis.Y) 0.5 + of * dir.stepY else random.nextDouble()
+                val z = if (axis === Direction.Axis.Z) 0.5 + of * dir.stepZ else random.nextDouble()
+                sendParticles(type, pos.x + x, pos.y + y, pos.z + z, 1, 0.0, 0.0, 0.0, 0.0)
+            }
+        }
+    }
 
 }
