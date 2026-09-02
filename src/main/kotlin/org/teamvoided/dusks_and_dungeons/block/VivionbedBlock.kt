71b8b2d0b674e37e30ab25c251a2d24fa4893b64
TheEnderCore <theendercore@gmail.com>
2026-09-02 19:27:39 +0300

    Clean up VivionbedBlock

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/block/VivionbedBlock.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/block/VivionbedBlock.kt
@@ -1,12 +1,15 @@
 package org.teamvoided.dusks_and_dungeons.block
 
-import net.minecraft.world.level.block.state.BlockState
-import net.minecraft.world.level.block.PinkPetalsBlock
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.block.PinkPetalsBlock
+import net.minecraft.world.level.block.state.BlockState
 import org.teamvoided.dusks_and_dungeons.data.tags.DnDBlockTags
 
-class VivionbedBlock(settings: Properties) : PinkPetalsBlock(settings) {
-    override fun mayPlaceOn(floor: BlockState, world: BlockGetter, pos: BlockPos): Boolean =
-        floor.`is`(DnDBlockTags.SUPPORTS_VIVIONBED)
+class VivionbedBlock(properties: Properties) : PinkPetalsBlock(properties) {
+
+    override fun mayPlaceOn(floor: BlockState, world: BlockGetter, pos: BlockPos): Boolean {
+        return floor.`is`(DnDBlockTags.SUPPORTS_VIVIONBED)
+    }
+
 }
