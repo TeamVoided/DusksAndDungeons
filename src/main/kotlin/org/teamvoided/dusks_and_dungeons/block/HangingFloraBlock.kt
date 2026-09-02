13e320c3cabca8dccec8273c2fef6522f4e68682
TheEnderCore <theendercore@gmail.com>
2026-09-02 11:44:31 +0300

    fixes HangingFloraBlock & cleans it up

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/block/HangingFloraBlock.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/block/HangingFloraBlock.kt
@@ -16,7 +16,8 @@ import net.minecraft.world.level.block.SimpleWaterloggedBlock
 import net.minecraft.world.level.block.SupportType
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
-import net.minecraft.world.level.block.state.properties.*
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.BooleanProperty
 import net.minecraft.world.level.material.FluidState
 import net.minecraft.world.level.material.Fluids
 import net.minecraft.world.level.pathfinder.PathComputationType
@@ -27,40 +28,28 @@ import org.teamvoided.dusks_and_dungeons.util.block.symmetricalBoxY
 class HangingFloraBlock(properties: Properties) : Block(properties), BonemealableBlock, SimpleWaterloggedBlock {
 
     init {
-        this.registerDefaultState(
-            this.stateDefinition.any().setValue(TIP, true).setValue(WATERLOGGED, false)
+        registerDefaultState(
+            stateDefinition.any()
+                .setValue(TIP, true)
+                .setValue(WATERLOGGED, false)
         )
     }
-
-    //public MapCodec<net.minecraft.world.level.block.BambooStalkBlock> codec() {
-    //    return CODEC;
-    //}
 
     override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
         builder.add(TIP, WATERLOGGED)
     }
 
-    override fun propagatesSkylightDown(blockState: BlockState, blockGetter: BlockGetter, blockPos: BlockPos): Boolean =
-        true
+    override fun isCollisionShapeFullBlock(blockState: BlockState, level: BlockGetter, pos: BlockPos): Boolean = false
 
-    override fun getShape(
-        blockState: BlockState,
-        blockGetter: BlockGetter,
-        blockPos: BlockPos,
-        collisionContext: CollisionContext
-    ): VoxelShape {
-        val voxelShape: VoxelShape = if (blockState.getValue(TIP)) TIP_SHAPE else SHAPE
-        val vec3 = blockState.getOffset(blockGetter, blockPos)
-        return voxelShape.move(vec3.x, vec3.y, vec3.z)
+    override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, ctx: CollisionContext): VoxelShape {
+        val shape = if (state.getValue(TIP)) TIP_SHAPE else SHAPE
+        val offset = state.getOffset(level, pos)
+        return shape.move(offset.x, offset.y, offset.z)
     }
 
-    override fun isPathfindable(blockState: BlockState, pathComputationType: PathComputationType): Boolean = false
+    override fun propagatesSkylightDown(state: BlockState, level: BlockGetter, pos: BlockPos): Boolean = true
 
-    override fun isCollisionShapeFullBlock(
-        blockState: BlockState,
-        blockGetter: BlockGetter,
-        blockPos: BlockPos
-    ): Boolean = false
+    override fun isPathfindable(state: BlockState, type: PathComputationType): Boolean = false
 
     override fun getStateForPlacement(ctx: BlockPlaceContext): BlockState? {
         return defaultBlockState()
@@ -68,95 +57,77 @@ class HangingFloraBlock(properties: Properties) : Block(properties), Bonemealabl
             .setValue(WATERLOGGED, ctx.level.getFluidState(ctx.clickedPos).type == Fluids.WATER)
     }
 
-    override fun canSurvive(blockState: BlockState, levelReader: LevelReader, blockPos: BlockPos): Boolean {
-        val abovePos = blockPos.above()
-        val aboveState = levelReader.getBlockState(abovePos)
+    override fun canSurvive(state: BlockState, level: LevelReader, pos: BlockPos): Boolean {
+        val abovePos = pos.above()
+        val aboveState = level.getBlockState(abovePos)
         return aboveState.`is`(this) ||
                 aboveState.`is`(BlockTags.LEAVES) ||
-                aboveState.isFaceSturdy(levelReader, abovePos, Direction.DOWN, SupportType.FULL)
+                aboveState.isFaceSturdy(level, abovePos, Direction.DOWN, SupportType.FULL)
     }
 
-    override fun tick( //onScheduledTick
-        blockState: BlockState,
-        serverLevel: ServerLevel,
-        blockPos: BlockPos,
-        randomSource: RandomSource
-    ) {
-        if (!blockState.canSurvive(serverLevel, blockPos))
-            serverLevel.destroyBlock(blockPos, true)
+    override fun tick(state: BlockState, level: ServerLevel, pos: BlockPos, random: RandomSource) {
+        if (!state.canSurvive(level, pos)) {
+            level.destroyBlock(pos, true)
+        }
     }
 
     override fun updateShape(
-        blockState: BlockState,
-        direction: Direction,
-        blockState2: BlockState,
-        level: LevelAccessor,
-        blockPos: BlockPos,
-        blockPos2: BlockPos
+        state: BlockState, dir: Direction, neighborState: BlockState,
+        level: LevelAccessor, pos: BlockPos, neighborPos: BlockPos,
     ): BlockState {
-        if (blockState.getValue(WATERLOGGED))
-            level.scheduleTick(blockPos, Fluids.WATER, Fluids.WATER.getTickDelay(level))
-        if (!blockState.canSurvive(level, blockPos))
-            level.scheduleTick(blockPos, this, 1)
-        return super.updateShape(blockState, direction, blockState2, level, blockPos, blockPos2)
-            .setValue(TIP, !level.getBlockState(blockPos.below()).`is`(this))
+        if (state.getValue(WATERLOGGED))
+            level.scheduleTick(pos, Fluids.WATER, Fluids.WATER.getTickDelay(level))
+        if (!state.canSurvive(level, pos))
+            level.scheduleTick(pos, this, 1)
+        return super.updateShape(state, dir, neighborState, level, pos, neighborPos)
+            .setValue(TIP, !level.getBlockState(pos.below()).`is`(this))
     }
 
     override fun getFluidState(state: BlockState): FluidState {
-        return if (state.getValue(WATERLOGGED)) Fluids.WATER.getSource(false)
-        else super.getFluidState(state)
+        return if (state.getValue(WATERLOGGED)) Fluids.WATER.getSource(false) else super.getFluidState(state)
     }
 
     //Valid means will consume item and display particles
     //Success means will run Perform
 
-    override fun isValidBonemealTarget(levelReader: LevelReader, blockPos: BlockPos, blockState: BlockState): Boolean {
-        val distToTop = this.getHeightAboveUpToMax(levelReader, blockPos)
-        val distToBot = this.getHeightBelowUpToMax(levelReader, blockPos)
-        return if (distToTop + distToBot + 1 < MAX_HEIGHT) false
-        else levelReader.getBlockState(blockPos.below(distToBot)).isAir
+    override fun isValidBonemealTarget(level: LevelReader, pos: BlockPos, state: BlockState): Boolean {
+        val distToTop = getHeightAboveUpToMax(level, pos)
+        val distToBot = getHeightBelowUpToMax(level, pos)
+        return (distToTop + distToBot + 1 < MAX_HEIGHT) && level.getBlockState(pos.below(distToBot + 1)).isAir
     }
 
-    override fun isBonemealSuccess(
-        level: Level,
-        randomSource: RandomSource,
-        blockPos: BlockPos,
-        blockState: BlockState
-    ): Boolean = true
+    override fun isBonemealSuccess(level: Level, random: RandomSource, pos: BlockPos, state: BlockState): Boolean = true
 
-    override fun performBonemeal(
-        serverLevel: ServerLevel,
-        randomSource: RandomSource,
-        blockPos: BlockPos,
-        blockState: BlockState
-    ) {
-        val bottom = blockPos.below(this.getHeightBelowUpToMax(serverLevel, blockPos))
-        serverLevel.setBlock(bottom, this.defaultBlockState(), UPDATE_CLIENTS)
+    override fun performBonemeal(level: ServerLevel, random: RandomSource, pos: BlockPos, state: BlockState) {
+        val bellow = pos.below(getHeightBelowUpToMax(level, pos) + 1)
+        level.setBlock(bellow, defaultBlockState(), UPDATE_ALL)
     }
 
-    fun getHeightAboveUpToMax(blockGetter: BlockGetter, blockPos: BlockPos): Int {
-        var i = 0
-        while (i < MAX_HEIGHT && blockGetter.getBlockState(blockPos.above(i + 1)).`is`(this)) {
-            ++i
+    fun getHeightAboveUpToMax(level: BlockGetter, pos: BlockPos): Int {
+        var up = 0
+        while (up < MAX_HEIGHT && level.getBlockState(pos.above(up + 1)).`is`(this)) {
+            ++up
         }
-        return i
+        return up
     }
 
-    fun getHeightBelowUpToMax(blockGetter: BlockGetter, blockPos: BlockPos): Int {
-        var i = 0
-        while (i < MAX_HEIGHT && blockGetter.getBlockState(blockPos.below(i + 1)).`is`(this)) {
-            ++i
+    fun getHeightBelowUpToMax(level: BlockGetter, pos: BlockPos): Int {
+        var down = 0
+        while (down < MAX_HEIGHT && level.getBlockState(pos.below(down + 1)).`is`(this)) {
+            ++down
         }
-        return i
+        return down
     }
-
 
     companion object {
-        val TIP_SHAPE: VoxelShape = symmetricalBoxY(2.0, 4.0, 16.0)
-        val SHAPE: VoxelShape = symmetricalBoxY(2.0, 0.0, 16.0)
+
         const val MAX_HEIGHT: Int = 16
 
+        val TIP_SHAPE: VoxelShape = symmetricalBoxY(2.0, 4.0, 16.0)
+        val SHAPE: VoxelShape = symmetricalBoxY(2.0, 0.0, 16.0)
+
         val TIP: BooleanProperty = BlockStateProperties.BOTTOM
-        val WATERLOGGED = BlockStateProperties.WATERLOGGED
+        val WATERLOGGED: BooleanProperty = BlockStateProperties.WATERLOGGED
+
     }
 }
