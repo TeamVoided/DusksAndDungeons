483487cdc34f97ae3855f7c454a0a4f7bc59c076
TheEnderCore <theendercore@gmail.com>
2026-09-10 22:13:37 +0300

    red sus sand now is attached to the block entity

--- src/main/kotlin/org/teamvoided/dusks_and_dungeons/init/DnDBlockEntities.kt
+++ src/main/kotlin/org/teamvoided/dusks_and_dungeons/init/DnDBlockEntities.kt
@@ -1,14 +1,16 @@
 package org.teamvoided.dusks_and_dungeons.init
 
+import net.minecraft.Util
+import net.minecraft.core.Registry
+import net.minecraft.core.registries.BuiltInRegistries
+import net.minecraft.util.datafix.fixes.References
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityType
-import net.minecraft.util.datafix.fixes.References
-import net.minecraft.core.registries.BuiltInRegistries
-import net.minecraft.core.Registry
-import net.minecraft.Util
 
 object DnDBlockEntities {
-    fun init() {}
+    fun init() {
+        BlockEntityType.BRUSHABLE_BLOCK.addSupportedBlock(DnDBlocks.SUSPICIOUS_RED_SAND)
+    }
     private fun <T : BlockEntity> register(id: String, builder: BlockEntityType.Builder<T>): BlockEntityType<T> {
         val type = Util.fetchChoiceType(References.BLOCK_ENTITY, id)
         return Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, id, builder.build(type))
