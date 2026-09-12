4e2280bee9f71ed331c5c1877f9123767b652cc8
TheEnderCore <theendercore@gmail.com>
2026-09-12 19:42:46 +0300

    pumpkin overlay mixin

---
+++ src/client/java/org/teamvoided/dusks_and_dungeons/mixin/GuiMixin.java
@@ -0,0 +1,30 @@
+package org.teamvoided.dusks_and_dungeons.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Blocks;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Gui.class)
+public abstract class GuiMixin {
+
+    @Shadow
+    protected abstract void renderTextureOverlay(GuiGraphics guiGraphics, ResourceLocation resourceLocation, float f);
+
+    @Inject(method = "renderCameraOverlays", at= @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;is(Lnet/minecraft/world/item/Item;)Z"))
+    void renderPumpkinOverlays(GuiGraphics guiGraphics, DeltaTracker deltaTracker, CallbackInfo ci, @Local ItemStack stack){
+        if (stack.is(Blocks.CARVED_PUMPKIN.asItem())) {
+            this.renderTextureOverlay(guiGraphics, PUMPKIN_BLUR_LOCATION, 1.0F);
+        }
+
+    }
+
+}
